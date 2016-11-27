1a7c42705c311ca4a9e16cd3746bf41830947007
James Sawyer <550961784@qq.com>
2016-11-27 14:41:49 +0800

    add some files

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return (
+      <h1>hello</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+)
